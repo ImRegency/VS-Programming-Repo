246df3c8678e52737b663244c01bd104b3c8a165
ImRegency <153047709+ImRegency@users.noreply.github.com>
2024-01-28 01:36:03 -0600

    Added Space Before User Input

--- Welcome.py
+++ Welcome.py
@@ -1,8 +1,8 @@
 # Get Users First Name
-first_name=input('Enter your first name:')
+first_name=input('Enter your first name: ')
 
 # Get Users Last Name
-last_name=input('Enter your last name:')
+last_name=input('Enter your last name: ')
 
 #Welcome User to Python
 print('Welcome to Python', first_name, last_name)
